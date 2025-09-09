9e6bf5c5ae31067903f1d82e6736e9d22ad420ea
sameena140 <shaiksameena053@gmail.com>
2025-09-09 11:39:09 +0530

    added multipe projects

---
+++ MINI PROJECT/Forensic analysis of a image detection/rewritecode.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import tkinter
+from tkinter import filedialog
+import numpy as np
+from tkinter.filedialog import askdirectory
+from tkinter import simpledialog
+import cv2
+from keras.utils.np_utils import to_categorical
+from keras.layers import Input
+from keras.models import Model
+from keras.layers import MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D
+from keras.models import Sequential
+import keras
+import pickle
+import matplotlib.pyplot as plt
+import os
+from keras.models import model_from_json
---
+++ MINI PROJECT/collegeapp/sections/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.Http import HttpResponse
+
+# Create your views here.
+def addCsea(request):
+    return HttpResponse("this is CSE A section")
+
